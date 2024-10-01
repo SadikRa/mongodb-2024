8b034d2a1759abd5b586eb2bf05d431bc7d0a5a0
SadikRa <sadikrahman494@gmail.com>
2024-10-01 22:17:42 +0600

    first post added done

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,87 +1,99 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-import { AuthContext } from "../../provider/AuthProvider";
+import { AuthContext } from "../provider/AuthProvider";
+// import { AuthContext } from "../../provider/AuthProvider";
 
 const Register = () => {
-
-    
   const { createUser } = useContext(AuthContext);
 
+  const handleRegister = (e) => {
+    e.preventDefault();
+    const name = e.target.name.value;
+    const email = e.target.email.value;
+    const password = e.target.password.value;
+    //   console.log(name, email, password);
+    const data = { name, email, password };
 
+    fetch("http://localhost:5000/users", {
+      method: "POST",
+      headers: {
+        "content-type": "application/json"
+      },
+      body: JSON.stringify(data),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
 
-  const handleRegister = e => {
-      e.preventDefault();
-      const name = e.target.name.value;
-      const email = e.target.email.value;
-      const password = e.target.password.value;
-      console.log(name, email, password);
-
-      // create user in firebase
-      createUser(email, password)
-          .then(result => {
-              console.log(result.user)
-          })
-          .catch(error => {
-              console.error(error);
-          })
-  }
-
+    // create user in firebase
+    createUser(email, password)
+      .then((result) => {
+        console.log(result.user);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
   return (
     <div className="hero bg-base-200 min-h-screen">
-    <div className="hero-content flex-col lg:flex-row-reverse">
-      <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-        <form onSubmit={handleRegister} className="card-body">
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Name</span>
-            </label>
-            <input
-              type="text"
-              name="name"
-              placeholder="Your name"
-              className="input input-bordered"
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Email</span>
-            </label>
-            <input
-              type="email"
-              name="email"
-              placeholder="email"
-              className="input input-bordered"
-              required
-            />
-          </div>
-          <div className="form-control">
-            <label className="label">
-              <span className="label-text">Password</span>
-            </label>
-            <input
-              type="password"
-              placeholder="password"
-              name="password"
-              className="input input-bordered"
-              required
-            />
-            <label className="label">
-              <a href="#" className="label-text-alt link link-hover">
-                Forgot password?
-              </a>
-            </label>
-          </div>
-          <div className="form-control mt-6">
-            <input className="btn btn-primary" type="submit" value="Register" />
-            <p>Already have an account? Please <Link to={'/login'}>Login</Link></p>
-          </div>
-        </form>
+      <div className="hero-content flex-col lg:flex-row-reverse">
+        <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+          <form onSubmit={handleRegister} className="card-body">
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Name</span>
+              </label>
+              <input
+                type="text"
+                name="name"
+                placeholder="Your name"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Email</span>
+              </label>
+              <input
+                type="email"
+                name="email"
+                placeholder="email"
+                className="input input-bordered"
+                required
+              />
+            </div>
+            <div className="form-control">
+              <label className="label">
+                <span className="label-text">Password</span>
+              </label>
+              <input
+                type="password"
+                placeholder="password"
+                name="password"
+                className="input input-bordered"
+                required
+              />
+              <label className="label">
+                <a href="#" className="label-text-alt link link-hover">
+                  Forgot password?
+                </a>
+              </label>
+            </div>
+            <div className="form-control mt-6">
+              <input
+                className="btn btn-primary"
+                type="submit"
+                value="Register"
+              />
+              <p>
+                Already have an account? Please <Link to={"/login"}>Login</Link>
+              </p>
+            </div>
+          </form>
+        </div>
       </div>
     </div>
-  </div>
-  
   );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,22 +1,45 @@
 import * as React from "react";
-import * as ReactDOM from "react-dom/client";
+// import * as ReactDOM from "react-dom/client";
 import {
   createBrowserRouter,
   RouterProvider,
 } from "react-router-dom";
 import "./index.css";
+import Register from "./components/Register/Register";
+import Login from "./components/Login/Login";
+// import AuthProvider from "./components/provider/AuthProvider";
+import Home from "./components/Home/Home";
+import AuthProvider from "./components/provider/AuthProvider";
+import { createRoot } from "react-dom/client";
+import { StrictMode } from "react";
+
+
 
 const router = createBrowserRouter([
   {
     path: "/",
-    element: <div>Hello world!</div>,
+    element: <Home />,
+    children: [
+      {
+        path: "/register",
+        element: <Register />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      }
+    ],
   },
 ]);
 
-ReactDOM.createRoot(document.getElementById("root")).render(
-  <React.StrictMode>
-   <div className="container mx-auto">
-   <RouterProvider router={router} />
-   </div>
-  </React.StrictMode>
+
+
+createRoot(document.getElementById("root")).render(
+  <StrictMode>
+    <div className="container mx-auto">
+      <AuthProvider>
+        <RouterProvider router={router} />
+      </AuthProvider>
+    </div>
+  </StrictMode>
 );
