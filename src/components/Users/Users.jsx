51a2ecebc7cc56594af52dcb069d82510e32270f
SadikRa <sadikrahman494@gmail.com>
2024-10-02 15:37:55 +0600

    users delete added done

--- src/components/Users/Users.jsx
+++ src/components/Users/Users.jsx
@@ -1,12 +1,28 @@
 import { useLoaderData } from "react-router-dom";
 
 const Users = () => {
-    const userData = useLoaderData()
-    return (
-        <div>
-            
+  const userData = useLoaderData();
+
+  const handleDelete = (_id) => {
+    fetch(`http://localhost:5000/users/${_id}`, {
+      method: "DELETE",
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data));
+  };
+  return (
+    <div className="grid grid-cols-1 md:grid-cols-3 gap-4">
+      {userData.map(({ _id, name, email }) => (
+        <div key={_id} className="card w-full bg-base-100 shadow-xl">
+          <div className="card-body">
+            <h2 className="card-title">{name}</h2>
+            <p>{email}</p>
+            <button onClick={() => handleDelete(_id)}>X</button>
+          </div>
         </div>
-    );
+      ))}
+    </div>
+  );
 };
 
 export default Users;
